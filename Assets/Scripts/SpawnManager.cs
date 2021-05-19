5e9c384ee3fdb8da08b28a19da7e64c246ba3688
Alec <aleconey@gmail.com>
2021-05-19 16:39:24 -0700

    spawn manager updates

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
 
     //Spawn enemies every 5 seconds
+    //create a coroutine IEnumerator -- yield events
+    //while loop
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            
+
+            Vector3 bounds = new Vector3(Random.Range(-9f, 9f), 7.5f, 0f);
+
+            GameObject newEnemy = Instantiate(_enemyPrefab, bounds, Quaternion.identity);
+
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            yield return new WaitForSeconds(5.0f);
+        }
+
+    }
+
 
 }
