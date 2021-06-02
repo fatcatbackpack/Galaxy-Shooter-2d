1b90987f6fdc2d399037cec9bf70ff40cc28ca50
Alec <aleconey@gmail.com>
2021-06-02 16:38:28 -0700

    finished speed boost

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
-    private GameObject _powerupPrefab;
+    private GameObject[] powerups;
     [SerializeField]
     private GameObject _enemyContainer;
 
@@ -46,9 +46,11 @@ public class SpawnManager : MonoBehaviour
         {
         Vector3 boundsPU = new Vector3(Random.Range(-9f, 9f), 7.5f, 0f);
 
+            int randomPowerUp = Random.Range(0, 2);
+
             yield return new WaitForSeconds(Random.Range(3, 8));
 
-            Instantiate(_powerupPrefab, boundsPU, Quaternion.identity);
+            Instantiate(powerups[randomPowerUp], boundsPU, Quaternion.identity);
 
 
         }
